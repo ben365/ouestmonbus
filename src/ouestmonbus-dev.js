60aa13ed43398358bd033d78e5f02121d19a50c1
Ben <benoit@systemd.info>
2017-01-13 23:10:17 +0100

    Try to use a personal cache for tiles server

--- src/ouestmonbus-dev.js
+++ src/ouestmonbus-dev.js
@@ -360,7 +360,7 @@ OuestmonbusApp.prototype.initMap = function() {
 	this.map.MAXBOUNDS = maxBounds;
 
 	// Ajout des tuiles
-	var tiles_layer = L.tileLayer("http://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png", {
+	var tiles_layer = L.tileLayer("https://tiles.ouestmonbus.com/osmfr/{z}/{x}/{y}.png", {
 		maxZoom: 19,
 		bounds: maxBounds,
 		opacity: 0.75
